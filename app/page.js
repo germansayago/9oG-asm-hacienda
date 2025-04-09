66112dff9a5cd00e23e3b927886bf784892922d4
Guillermo Rodriguez <dg.guillermo.rodriguez@gmail.com>
2025-04-09 07:20:03 -0300

    Se agrego la sección institucional "About"

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Auctions from "./ui/Auctions";
 import Contact from "./ui/Contact";
 import Cta from "./ui/Cta";
+import About from "./ui/About";
 import Gallery from "./ui/Gallery";
 import Hero from "./ui/Hero";
 
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero />
       <Auctions />
       <Cta />
+      <About />
       <Gallery />
       <Contact />
     </div>
