25fdd6c6277b293f0631056eeb651cd2ef0f8499
German Sayago <german.sayago@gmail.com>
2025-04-17 23:10:34 -0300

    [debug] se agrego log para ver imagenes desde api

--- app/ui/Gallery.jsx
+++ app/ui/Gallery.jsx
@@ -8,13 +8,20 @@ export default async function Gallery() {
   // Unificar todas las imágenes cargadas en un único array
   const images = entries.flatMap((entry) => entry.fields.images || []);
 
+  console.log(images);
+
   return (
     <section className="gallery bg-light">
       <div className="container">
         <div className="row justify-content-center mb-4">
           <div className="col-md-6 text-center">
-            <h2 className="mb-4 text-primary">Resultados que hablan por sí mismos</h2>
-            <p className="lead mb-4">Más de tres décadas de experiencia se traducen en operaciones exitosas para nuestros clientes.</p>
+            <h2 className="mb-4 text-primary">
+              Resultados que hablan por sí mismos
+            </h2>
+            <p className="lead mb-4">
+              Más de tres décadas de experiencia se traducen en operaciones
+              exitosas para nuestros clientes.
+            </p>
           </div>
         </div>
 
@@ -23,7 +30,12 @@ export default async function Gallery() {
 
         <div className="row justify-content-center mt-5 mb-4">
           <div className="col-md-6 text-center">
-            <h5 className=""><span className="fw-bold">Contactanos</span> y descubrí por qué productores y compradores confían en <span className="fw-bold">Alfredo S. Modino</span> para sus transacciones ganaderas.</h5>
+            <h5 className="">
+              <span className="fw-bold">Contactanos</span> y descubrí por qué
+              productores y compradores confían en{" "}
+              <span className="fw-bold">Alfredo S. Modino</span> para sus
+              transacciones ganaderas.
+            </h5>
           </div>
         </div>
       </div>
